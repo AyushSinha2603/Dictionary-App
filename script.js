653eec0faaf1ed730a6f07278e30600062dcffa7
AyushSinha2603 <ayushcodes26@gmail.com>
2025-01-21 21:06:03 +0530

    Updated script

--- script.js
+++ script.js
@@ -7,31 +7,41 @@ form.addEventListener('submit', (e)=>{
 });
 
 const getWordInfo = async(word)=>{
-  const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-  const data = await response.json();
-  let definitions = data[0].meanings[0].definitions[0];
+    try{
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    const data = await response.json();
+    let definitions = data[0].meanings[0].definitions[0];
 
 
-  resultDiv.innerHTML = `
-      <h2><strong>Word:</strong>${data[0].word}
-      </h2>
-      <p>${data[0].meanings[0].partOfSpeech}
-      </p>
-      <p><strong>Meaning:</strong>${definitions.definition}
-      </p>
-      <p><strong>Example:</strong>${definitions.example === undefined ? "Not Found" : definitions.example}
-      </p>
-      <p><strong>Antonyms:</strong></p>
-  `;
+    resultDiv.innerHTML = `
+        <h2><strong>Word:</strong>${data[0].word}
+        </h2>
+        <p>${data[0].meanings[0].partOfSpeech}
+        </p>
+        <p><strong>Meaning:</strong>${definitions.definition}
+        </p>
+        <p><strong>Example:</strong>${definitions.example === undefined ? "Not Found" : definitions.example}
+        </p>
+        <p><strong>Antonyms:</strong></p>
+    `;
 
-  //for antonyms
-  if(definitions.antonyms.length ===0){
-    resultDiv.innerHTML += `<p>Not Found</p>`;
-  }
-  else{
-    for(let i=0;i<definitions.antonyms.length; i++){
-      resultDiv.innerHTML += `<li>${definitions.antonyms[i]}</li>`
+    //for antonyms
+    if(definitions.antonyms.length ===0){
+      resultDiv.innerHTML += `<p>Not Found</p>`;
     }
-  }
-  
+    else{
+      for(let i=0;i<definitions.antonyms.length; i++){
+        resultDiv.innerHTML += `<li>${definitions.antonyms[i]}</li>`
+      }
+    }
+    
+
+    //Adding read more button
+    resultDiv.innerHTML += `<div><a href="${data[0].sourceUrls}" target="_blank">Read More</a></div>`;
+    }
+    catch(error){
+      resultDiv.innerHTML = `<p>Sorry, the word could not be found</p>`
+    }
+
+
 }
