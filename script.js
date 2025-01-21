b33a8baa5b9c71a9003b4f6961e740e54f96513b
AyushSinha2603 <ayushcodes26@gmail.com>
2025-01-21 17:27:08 +0530

    script in progress

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form');
+const resultDiv = document.querySelector('.result');
+
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  getWordInfo(form.elements[0].value)
+});
+
+const getWordInfo = async(word)=>{
+  const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+  const data = await response.json();
+  console.log(data);
+}
