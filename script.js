6c5503101d658cb566f1e921039dd141ec3e6240
AyushSinha2603 <ayushcodes26@gmail.com>
2025-01-21 17:41:14 +0530

    update

--- script.js
+++ script.js
@@ -9,5 +9,19 @@ form.addEventListener('submit', (e)=>{
 const getWordInfo = async(word)=>{
   const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
   const data = await response.json();
+
+
+  resultDiv.innerHTML = `
+  <h2><strong>Word:</strong>${data[0].word}
+  </h2>
+  <p>${data[0].meanings[0].partOfSpeech}
+  </p>
+  <p><strong>Meaning:</strong>${data[0].meanings[0].definitions[0].definition}
+  </p>
+  <p><strong>Example:</strong>${data[0].meanings[0].definitions[0].example}
+  </p>
+  `;
+
+
   console.log(data);
 }
