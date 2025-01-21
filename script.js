ea0b37d56c20142988c8d8288f7af571c153270a
AyushSinha2603 <ayushcodes26@gmail.com>
2025-01-21 20:56:18 +0530

    antonyms added

--- script.js
+++ script.js
@@ -9,19 +9,29 @@ form.addEventListener('submit', (e)=>{
 const getWordInfo = async(word)=>{
   const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
   const data = await response.json();
+  let definitions = data[0].meanings[0].definitions[0];
 
 
   resultDiv.innerHTML = `
-  <h2><strong>Word:</strong>${data[0].word}
-  </h2>
-  <p>${data[0].meanings[0].partOfSpeech}
-  </p>
-  <p><strong>Meaning:</strong>${data[0].meanings[0].definitions[0].definition}
-  </p>
-  <p><strong>Example:</strong>${data[0].meanings[0].definitions[0].example}
-  </p>
+      <h2><strong>Word:</strong>${data[0].word}
+      </h2>
+      <p>${data[0].meanings[0].partOfSpeech}
+      </p>
+      <p><strong>Meaning:</strong>${definitions.definition}
+      </p>
+      <p><strong>Example:</strong>${definitions.example === undefined ? "Not Found" : definitions.example}
+      </p>
+      <p><strong>Antonyms:</strong></p>
   `;
 
-
-  console.log(data);
+  //for antonyms
+  if(definitions.antonyms.length ===0){
+    resultDiv.innerHTML += `<p>Not Found</p>`;
+  }
+  else{
+    for(let i=0;i<definitions.antonyms.length; i++){
+      resultDiv.innerHTML += `<li>${definitions.antonyms[i]}</li>`
+    }
+  }
+  
 }
